b2bb3818c47002533f9494959626526ad8ea1cf8
shubhamyyadav <shubhamyyadav@deloitte.com>
2024-03-15 12:30:26 +0530

    Git Actions Changes

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "d7yevq",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
